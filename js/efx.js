315f601c40f1b713e50a42bb54b2ee9479bb1129
ElJohnnie <johnfollmer1995@gmail.com>
2021-08-06 14:31:43 -0300

    remoção de alguns códigos que estavam no projeto original

--- js/efx.js
+++ js/efx.js
@@ -2,18 +2,14 @@
 (function($) {
     var $window = $(window),
         $con = $('#main-container');
-
     $window.resize(function resize() {
         if ($window.width() <= 1200) {
             return $con.removeClass('col-lg-6');
         }
-
         $con.addClass('col-lg-6');
     }).trigger('resize');
 })(jQuery);
 //fim tratamento col-lg
-
-
 
 //efeito ancora na seleção do menu
 (function() {
@@ -38,11 +34,8 @@
         $('html, body').animate({
             scrollTop: targetOffset - 0
         }, 1000);
-
     });
 })();
-
-//efeito ancora para o prr
 
 
 //efeito return to top com transição
@@ -63,51 +56,3 @@ $(function() {
         $con.addClass('background-fixed');
     });
 });
-
-$(function() {
-    $('#bobinas').on('hide.bs.collapse', function(e) {
-        $('html,body').animate({
-            scrollTop: $('#produtos').offset().top - 0
-        }, 500);
-        $con.removeClass('background-fixed');
-        $con.addClass('background-wrapper');
-    });
-});
-
-$("#bobinas").click(function() {
-
-});
-
-
-
-
-
-//parollers
-$("#sect-subtitle-institucional").paroller({
-    factor: 0.5,
-    factorXs: 0.2,
-    type: 'foreground',
-    direction: 'vertical'
-});
-
-$("#sect-subtitle-produtos").paroller({
-    factor: 0.5,
-    factorXs: 0.2,
-    type: 'foreground',
-    direction: 'vertical'
-});
-
-$("#sect-subtitle-ligas").paroller({
-    factor: 0.5,
-    factorXs: 0.2,
-    type: 'foreground',
-    direction: 'vertical'
-});
-
-$("#sect-subtitle-servicos").paroller({
-    factor: 0.5,
-    factorXs: 0.2,
-    type: 'foreground',
-    direction: 'vertical'
-});
-//fim parollers
